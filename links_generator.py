6a0745621f2cdeca9849d52602c82bad59019200
Gaurav Kashyap <gauravandkashyap@gmail.com>
2019-02-03 14:09:07 +0530

    Added Signup process. Extracting HTML made automatic.

--- links_generator.py
+++ links_generator.py
@@ -1,8 +1,51 @@
+from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+from time import sleep, time
 from bs4 import BeautifulSoup
+from selenium.common.exceptions import NoSuchElementException, WebDriverException
 
-with open('raw_html_utf-8.txt', 'r', encoding='utf-8') as f:
-    file_soup = BeautifulSoup(f.read(), 'lxml')
+driver = webdriver.Chrome('D:/Download/chromedriver_win32/chromedriver.exe')
+driver.implicitly_wait(5)
+# get user to login
+driver.get('https://youtube.com')
+try:
+    driver.find_element_by_css_selector('#buttons > ytd-button-renderer > a').click()
 
-links = file_soup.select('a.pl-video-title-link.yt-uix-tile-link.yt-uix-sessionlink.spf-link ')
+    # print("Enter \"done\" when you're done logging in")
 
-print(len(links))
+    while driver.current_url[:23] != "https://www.youtube.com":
+        print(driver.current_url, driver.current_url[:23])
+        sleep(2)
+
+    driver.maximize_window()  # to view the liked videos section
+
+    driver.find_element_by_xpath('//*[@title="Liked videos"]').click()  # goes to likes page
+    driver.get(str(driver.current_url) + "&disable_polymer=true")  # takes to legacy page where the below class selections work
+
+    # edit these variable if all links aren't loaded as per internet speed etc
+    loop_time = 60 * 6  # in seconds
+    internet_buffer_time = 2
+
+    time_end = time() + loop_time
+    while time() < time_end:
+        try:
+            driver.find_element_by_tag_name('html').send_keys(Keys.END)
+            driver.find_element_by_css_selector('#pl-video-list > button').click()
+            sleep(internet_buffer_time)
+        except NoSuchElementException:
+            break
+
+    sleep(2)  # give a second to calm tf down
+    html = driver.page_source
+
+    with open('liked_videos_raw_html.txt', 'w', encoding='utf-8') as f:
+        f.write(html)
+
+    with open('liked_videos_raw_html.txt', 'r', encoding='utf-8') as f:
+        file_soup = BeautifulSoup(f.read(), 'lxml')
+
+    links = file_soup.select('a.pl-video-title-link.yt-uix-tile-link.yt-uix-sessionlink.spf-link ')
+    
+    print(len(links))
+except WebDriverException:
+    print("Window Closed! Ending process...")
