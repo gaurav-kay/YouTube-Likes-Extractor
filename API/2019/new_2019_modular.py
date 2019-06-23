fdf2a2d1cf8f7b4dd93c4c9347e59d3f3e3d0f5c
Gaurav Kashyap <gauravandkashyap@gmail.com>
2019-06-23 17:17:49 +0530

    nextPageToken works with a paid GCP account to make more than 20 API calls

--- API/2019/new_2019_modular.py
+++ API/2019/new_2019_modular.py
@@ -1,4 +1,6 @@
-# todo: okay so nextPageToken isn't returning all the videos.
+# todo: ~okay so nextPageToken isn't returning all the videos.~
+# todo: verify that more than 5000 videos are recorded
+# requires a paid GCP account to work properly (CLIENT_SECRET)
 
 import google_auth_oauthlib.flow
 import googleapiclient.discovery
@@ -6,8 +8,12 @@ import googleapiclient.errors
 
 import os
 import json
+from time import sleep
 
-scopes = ["https://www.googleapis.com/auth/youtube.readonly"]
+scopes = ["https://www.googleapis.com/auth/youtube"]
+parts = "snippet"
+items = []
+total = 0
 
 
 def get_authorised_youtube():
@@ -15,7 +21,7 @@ def get_authorised_youtube():
 
     api_service_name = "youtube"
     api_version = "v3"
-    client_secrets_file = "LOCATION OF CLIENT_SECRET_FILE.JSON"
+    client_secrets_file = "CLIENT_SECRET_FILE.JSON"
 
     flow = google_auth_oauthlib.flow.InstalledAppFlow.from_client_secrets_file(
                 client_secrets_file,
@@ -34,13 +40,13 @@ def get_authorised_youtube():
 def make_single_request(youtube, nextPageToken):
     if nextPageToken is None:
         request = youtube.videos().list(
-            part="snippet",
+            part=parts,
             myRating="like",
             maxResults=50
         )
     else:
         request = youtube.videos().list(
-            part="snippet",
+            part=parts,
             myRating="like",
             pageToken=nextPageToken,
             maxResults=50
@@ -50,11 +56,22 @@ def make_single_request(youtube, nextPageToken):
     return response
 
 
+def write(response):
+    for item in response["items"]:
+        with open('test1.json', 'a', encoding='utf-8') as fp:
+            print(json.dumps(item, sort_keys=True, indent=4, ensure_ascii=False), file=fp)  # god. prints utf-8 prettified json
+    global total
+    total += len(response["items"])
+    print(total)
+
+
 def main():
     youtube = get_authorised_youtube()
 
     first_response = make_single_request(youtube, None)
     nextPageToken = first_response["nextPageToken"]
+
+    write(first_response)
 
     try:
         count = 0
@@ -62,12 +79,24 @@ def main():
             count += 1
             print(count, end=" ")
             response = make_single_request(youtube, nextPageToken)
+            write(response)
             nextPageToken = response["nextPageToken"]
             print(nextPageToken)
-    except KeyError as e:
-        response.pop("items")
+
+            # items.append(item for item in response["items"])
+            # with open("final.txt", 'a') as f:
+            #     lines = '\n'.join(items)
+            #     f.write(lines)
+
+            sleep(1)
+
+    except KeyError:
+        write(response)
+        response.pop("items", None)
+
         print(response)
 
 
 if __name__ == '__main__':
     main()
+
